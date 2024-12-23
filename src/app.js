b9e65fc517fd5bad3752581df550cee8811b68b4
surajbhoji1208 <60956130+surajbhoji1208@users.noreply.github.com>
2024-12-23 22:16:09 +0530

    EP3 initialize project

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.use((req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is listening on port 3000");
+})
